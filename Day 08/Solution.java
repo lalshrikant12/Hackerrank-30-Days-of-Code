25ab345f45e42d48bef6a812bb462635b72924b3
Shri Kant Lal <46701610+lalshrikant12@users.noreply.github.com>
2020-06-05 20:19:30 +0530

    Create Solution.java

---
+++ Day 08/Solution.java
@@ -0,0 +1,31 @@
+//Complete this code or write your own from scratch
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+         Map< String,Integer> hm =  
+                        new HashMap< String,Integer>(); 
+        int n = in.nextInt();
+        for(int i = 0; i < n; i++){
+            String name = in.next();
+            int phone = in.nextInt();
+            hm.put(name,phone);
+            // Write code here
+        }
+        while(in.hasNext()){
+            String s = in.next();
+            // Write code here
+            if(hm.containsKey(s))
+            {
+            System.out.println(s + "=" + hm.get(s));
+            }
+            else
+            {
+                System.out.println("Not found");
+            }
+        }
+        in.close();
+    }
+}
